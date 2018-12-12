9a66de55d6cc3878504dd0586b8ef104bef4085d
KYLIAN-PC\KYLIAN <kylianleemkuil@hotmail.com>
2018-12-12 15:47:32 +0100

    Made a begin with the event page

--- Webapp/src/components/Events/Event.vue
+++ Webapp/src/components/Events/Event.vue
@@ -23,15 +23,76 @@
             </b-row>
             <b-row class="mt-3">
               <b-col class="opacity-text-8" md="12">
-                {{event.description}}
+                {{event.small_description}}
               </b-col>
             </b-row>
           </b-col>
         </b-row>
       </b-container>
     </b-row>
-    <b-row style="margin-top: -50px;">
-      
+    <b-row style="margin-top: -80px;">
+      <b-container>
+        <b-row>
+            <b-col md="7">
+                <b-card class="no-scale">
+                    {{event.description}}
+                </b-card>
+            </b-col>
+            <b-col>
+                <b-card class="no-scale">
+                    <b-row>
+                        <b-col> 
+                            <ins class="font-weight-bold">@ {{event.location_name}}</ins>
+                        </b-col>
+                    </b-row>
+                    <b-row>
+                        <b-col> 
+                             <span>{{event.location_street}} {{event.location_number}} {{event.location_number_suffix}}</span>
+                        </b-col>
+                    </b-row>
+                    <b-row>
+                        <b-col> 
+                             <span>{{event.location_postalcode}}</span>
+                        </b-col>
+                    </b-row>
+                    <b-row>
+                        <b-col> 
+                             <span>{{event.location_city}}</span>
+                        </b-col>
+                    </b-row>
+                    <b-row class="mt-2">
+                        <b-col class="text-center">
+                            <button v-if="true" type="button" style="width:100%;"
+                                    class="btn btn-ceecee-red text-center">
+                                Sign up for this event!
+                            </button>
+                            <button v-else type="button" style="width:100%;"
+                                    class="btn btn-ceecee-red text-center">
+                                Unsubscribe
+                            </button>
+                    </b-col>
+                    </b-row>
+                </b-card>
+            </b-col>
+        </b-row>
+        <b-row v-if="event.show_attendees" >
+            <b-col>
+                 <b-card class="no-scale">
+                     <b-row>
+                         <b-col>
+                            <b-img rounded="circle" class="image-profile-chips" width="100" height="100" src="https://images.pexels.com/photos/555790/pexels-photo-555790.png?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"></b-img>
+                         </b-col>
+                         <b-col>
+                            <b-img rounded="circle" class="image-profile-chips" width="100" height="100" src="https://images.pexels.com/photos/634021/pexels-photo-634021.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"></b-img>
+                         </b-col>
+                         <b-col>
+                            <b-img rounded="circle" class="image-profile-chips" width="100" height="100" src="https://images.pexels.com/photos/209790/pexels-photo-209790.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"></b-img>
+                         </b-col>
+                     </b-row>
+                 </b-card>
+            </b-col>
+        </b-row>
+      </b-container>
     </b-row>
   </b-container>
 </template>
@@ -58,7 +119,7 @@
             if(dateString && start){
                 return moment(String(dateString)).format('hh:mm')
             } else {
-                return moment(String(dateString)).format('hh:mm - MM MMMM')
+                return moment(String(dateString)).format('hh:mm @ MM MMMM YYYY')
             }
         }
     }
@@ -73,5 +134,9 @@
 
   .image-border {
         border:2px solid #E60000;
+  }
+
+  .image-profile-chips {
+      box-shadow: 0 10px 20px rgba(0, 0, 0, 0.3); 
   }
 </style>
