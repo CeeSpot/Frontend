b95bcd27cd3af790c0e97246ac03aa661d89e550
Frank Grevelink <fgrevelink@gmail.com>
2018-12-17 20:43:01 +0100

    Spaces design

---
+++ Webapp/src/components/Spaces/Space.vue
@@ -0,0 +1,76 @@
+<template>
+    <b-container style="margin-top: 140px;">
+        <b-row>
+            <b-col md="8">
+                <h1>Nice space</h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquam aliquid assumenda beatae
+                    consequatur deleniti deserunt doloribus est eveniet in, ipsa iste laudantium magni, neque nulla,
+                    perspiciatis provident quis voluptatibus.
+                </p>
+            </b-col>
+        </b-row>
+        <b-row>
+            <b-col md="8">
+                <img src="https://picsum.photos/600/300/?image=1" alt="Title" class="rounded shadow" style="width: 100%;">
+            </b-col>
+            <b-col md="4">
+                <b-row>
+                    <b-col>
+                        <div style="width: 100%; height: 100%; padding: 15px; font-size: 1.125em;" class="card shadow no-scale">
+                            <ul class="list-unstyled">
+                                <li><h3>Specifications</h3></li>
+                                <li>1 pc</li>
+                                <li>1 projector</li>
+                                <li>12 seats</li>
+                                <li></li>
+                            </ul>
+                        </div>
+                    </b-col>
+                </b-row>
+                <b-row class="mt-4">
+                    <b-col>
+                        <div style="width: 100%; height: 100%; padding: 15px; font-size: 1.125em;" class="card shadow no-scale">
+                            <h3>Reserve space</h3>
+                            <datetime  type="datetime" input-class="form-control" placeholder="Date and time" format="dd-MM-yyyy HH:mm:ss"></datetime>
+                            <b-button class="btn-block btn-ceecee-red mt-2">
+                                Reserve space
+                            </b-button>
+                        </div>
+                    </b-col>
+                </b-row>
+            </b-col>
+        </b-row>
+        <b-row>
+            <b-col md="8">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. In quaerat quidem saepe! Commodi doloribus
+                    excepturi incidunt officiis repudiandae! Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Ducimus eius neque quae reprehenderit totam voluptas. Illum ipsam non odio.
+                </p>
+            </b-col>
+        </b-row>
+    </b-container>
+</template>
+
+<script>
+  export default {
+    name: "Space",
+    created() {
+      this.id = this.$route.params.id;
+    },
+    mounted() {
+        // space inladen via api
+        this.space = '';
+    },
+    data() {
+      return {
+        space: null
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- Webapp/src/router/index.js
+++ Webapp/src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Home from '@/components/Landingpage/Home' // Home base page
 import Community from '@/components/Community/Community' // Community base page
 import Spaces from '@/components/Spaces/Spaces' // Spaces base page
+import Space from '@/components/Spaces/Space' // Space base page
 import Events from '@/components/Events/Events' // Events base page
 import Event from '@/components/Events/Event' // Event base page
 import Contact from '@/components/Contact/Contact' // Contact base page
@@ -47,6 +48,11 @@ export default new Router({
       path: '/spaces',
       name: 'Spaces',
       component: Spaces
+    },
+    {
+      path: '/spaces/:id',
+      name: 'Space',
+      component: Space
     },
     {
       path: '/events',
