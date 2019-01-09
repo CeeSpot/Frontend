2f0f0331fc6c355920e16853f1229303caa5a3f4
Kylian <kylianleemkuil@hotmail.com>
2019-01-09 12:39:09 +0100

    Added default template for component

---
+++ Webapp/src/components/Blog/Blogs.vue
@@ -0,0 +1,21 @@
+<template>
+
+</template>
+<script>
+import Vue from 'vue'
+
+  export default {
+    name: 'Blogs',
+    data() {
+      return {
+      }
+    },
+    methods: {
+    },
+    mounted() {
+    }
+  }
+</script>
+<style scoped>
+
+</style>
