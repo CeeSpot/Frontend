023988928fe4276a4f5f608dedf4fba2c3789b41
Stijn <thaman663@gmail.com>
2019-01-09 12:47:00 +0100

    Validator

--- Webapp/src/components/Core/Forms/RegisterForm.vue
+++ Webapp/src/components/Core/Forms/RegisterForm.vue
@@ -44,11 +44,15 @@
                 <b-form-group id="emailGroup"
                               label="Email"
                               label-for="email">
-                    <b-form-input id="email"
+                    <b-form-input v-on:keyup.native="checkEmail" id="email"
                                   type="email"
+                                  v-bind:style="{borderColor: emailCorrectColor}"
                                   v-model="form.email"
                                   required>
                     </b-form-input>
+                    <small id="passwordGroup__BV_description_" class="form-text">
+                        <span v-if="!emailCorrect && emailCorrect != null" class="text-danger">Email doesn't have the right format</span>
+                    </small>
                 </b-form-group>
             </b-col>
         </b-row>
@@ -69,12 +73,31 @@
             <b-col md="10" offset-md="1">
                 <b-form-group id="passwordGroup"
                               label="Password"
-                              label-for="password">
-                    <b-form-input id="password"
+                              label-for="password"
+                              >
+                    <b-form-input v-on:keyup.native="passwordsMatch"  id="password"
                                   type="password"
                                   v-model="form.password"
                                   required>
                     </b-form-input>
+                </b-form-group>
+            </b-col>
+        </b-row>
+        <b-row>
+            <b-col md="10" offset-md="1">
+                <b-form-group id="passwordGroupRepeat"
+                              label="Repeat Password"
+                              label-for="passwordRepeat">
+                    <b-form-input v-on:keyup.native="passwordsMatch" id="passwordRepeat"
+                                  type="password"
+                                  v-bind:style="{borderColor: passwordsMatchColor}"
+                                  v-model="form.passwordRepeat"
+                                  required>
+                    </b-form-input>
+                    <small id="passwordGroup__BV_description_" class="form-text">
+                        <span v-if="passwordMatch" class="text-success">Passwords match</span>
+                        <span v-if="!passwordMatch && passwordMatch != null" class="text-danger">Passwords do not match</span>
+                    </small>
                 </b-form-group>
             </b-col>
         </b-row>
@@ -97,11 +120,17 @@
                 form: {
                     username: '',
                     password: '',
+                    passwordRepeat: '',
                     firstname: '',
                     insertions: '',
                     lastname: '',
                     email: '',
                 },
+                passwordMatch: null,
+                passwordsMatchColor: '#ced4da',
+
+                emailCorrect: null,
+                emailCorrectColor: '#ced4da',
                 registerFailedMessage: ''
 //                token : ''
             }
@@ -125,6 +154,28 @@
                     console.log(resp);
                     this.registerFailedMessage = resp.data.message;
                 });
+            },
+            passwordsMatch: function(event) {
+                if(this.form.password===this.form.passwordRepeat){
+                    this.passwordsMatchColor = '#28a745';
+                    this.passwordMatch = true;
+                }else{
+                    this.passwordsMatchColor = '#bd2130';
+                    this.passwordMatch = false;
+                }
+            },
+            checkEmail: function(event) {
+                if(this.validateEmail(this.form.email)){
+                    this.emailCorrectColor = '#28a745';
+                    this.emailCorrect = true;
+                }else{
+                    this.emailCorrectColor = '#bd2130';
+                    this.emailCorrect = false;
+                }
+            },
+            validateEmail(email){
+                var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                return re.test(email);
             }
         }
     }
