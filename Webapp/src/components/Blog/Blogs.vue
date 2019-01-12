5ff668360259e358e0685b1ccc12fa556ad15b3a
KYLIAN-PC\KYLIAN <kylianleemkuil@hotmail.com>
2019-01-12 13:42:29 +0100

    Finished the blogs overview page

--- Webapp/src/components/Blog/Blogs.vue
+++ Webapp/src/components/Blog/Blogs.vue
@@ -1,29 +1,57 @@
 <template>
   <b-container class="container-margin">
-      <b-row>
-        <b-col md="4">
-          <b-input-group class="form-group-search">
-              <b-form-input v-model="search" :placeholder="$t('events.search')"
-                              class="form-control"></b-form-input>
-              <span class="form-control-icon">
-                  <font-awesome-icon icon="search" class="search-color"/>
-              </span>
-          </b-input-group>
-        </b-col>
-      </b-row>
       <b-row class="mt-3">
-          <b-col md="4" v-for="blog in blogList">
-              <b-card
-                      :title="blog.title"
-                      img-src="https://images.pexels.com/photos/1496183/pexels-photo-1496183.jpeg?cs=srgb&dl=adult-article-assortment-1496183.jpg&fm=jpg"
-                      img-top>
-                  <p class="card-text description">
-                      {{blog.description}}
-                  </p>
-                              <div slot="footer">
-                <small class="text-muted">{{formatDate(blog.date_created)}}</small>
-            </div>
-              </b-card>
+            <b-col md="8" class="mr-3">
+               <b-card v-for="blog in filterSearchAndTags()">
+                    <b-row>
+                    <b-col md="9">
+                        <div style="padding: 15px">
+                            <h3 class="bold">{{blog.title}}</h3>
+                            <p class="description">{{blog.description}}</p>
+                            <small class="text-muted">{{formatDate(blog.date_created)}}</small>
+                        </div>
+                    </b-col>
+                    <b-col>
+                         <b-img class="card-image" src="https://images.pexels.com/photos/1496183/pexels-photo-1496183.jpeg?cs=srgb&dl=adult-article-assortment-1496183.jpg&fm=jpg" fluid-grow/>
+                    </b-col>
+                </b-row>
+                </b-card>
+           </b-col>
+          <b-col>
+            <b-row>
+                <div style="width: 100%; padding: 15px; font-size: 1.125em;" class="card shadow no-scale">
+                    <h3>Zoeken</h3>
+                    <hr>
+                    <b-row>
+                        <b-col>
+                            <b-input-group class="form-group-search">
+                                <b-form-input v-model="search" :placeholder="$t('events.search')"
+                                                class="form-control"></b-form-input>
+                                <span class="form-control-icon">
+                                    <font-awesome-icon icon="search" class="search-color"/>
+                                </span>
+                            </b-input-group>
+                        </b-col>
+                    </b-row>
+                </div>
+            </b-row>
+            <b-row>
+                <div style="width: 100%; padding: 15px; font-size: 1.125em;" class="card shadow no-scale">
+                    <h3>Tags</h3>
+                    <hr>
+                    <b-row>
+                        <b-col>
+                            <ul>
+                                <li v-for="tag in tags">
+                                    <div :id="'tag' + tag.id" v-on:click="updateSelectedTags(tag.id);filterSearchAndTags();"
+                                        class="btn-ceecee-oval-red">{{tag.description}}
+                                    </div>
+                                </li>
+                            </ul>
+                        </b-col>
+                    </b-row>
+                </div>
+            </b-row>
           </b-col>
       </b-row>
   </b-container>
@@ -37,16 +65,49 @@ import moment from 'moment'
     data() {
       return {
         blogs: [],
-        search: ''
+        tags: [],
+        search: '',
+        selectedTags: []
       }
     },
     methods: {
       formatDate(date) {
             return moment(date).format('MMMM DD, YYYY')
+      },
+     updateSelectedTags: function (id) {
+        let button = document.getElementById("tag" + id);
+        if (button.classList.contains('btn-ceecee-oval-red-active')) {
+          button.classList.remove("btn-ceecee-oval-red-active");
+          this.selectedTags.splice(this.selectedTags.indexOf(id), 1);
+        } else {
+          button.classList.add("btn-ceecee-oval-red-active");
+          this.selectedTags.push(id);
+        }
+      },
+      filterSearchAndTags() {
+        let newBlogsList = [];
+        let filteredTags = 0;
+        this.blogList.forEach(blog => {
+          if (blog.tags.length > 0) {
+            blog.tags.forEach(tag => {
+              if (this.selectedTags.includes(tag.id)) {
+                if (!newBlogsList.includes(blog)) {
+                  newBlogsList.push(blog);
+                  filteredTags++;
+                }
+              }
+            });
+          }
+        })
+        if (this.selectedTags.length === 0) {
+          newBlogsList = this.blogList;
+        }
+        return newBlogsList;
       }
     },
     mounted() {
        blogApi.getBlogs().then(response => this.blogs = response.data.data);
+       blogApi.getBlogsTags().then(response => this.tags = response.data.data);
     },
     computed: {
         blogList() {
@@ -88,12 +149,38 @@ import moment from 'moment'
     .description {
      overflow: hidden;
      display: -webkit-box;
-     -webkit-line-clamp: 3;
+     -webkit-line-clamp: 2;
      -webkit-box-orient: vertical;
     }
 
     .card-title {
         font-weight: bold;
         font-size: 22px;
+    }
+
+    ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        overflow: hidden;
+    }
+
+    li {
+        float: left;
+        margin-top: 5px;
+    }
+
+    .card-image {
+          background-repeat: no-repeat;
+          background-size: auto;
+          height: 100%;
+    }
+
+    .blog-card {
+         cursor: pointer;
+    }
+
+    .card-body {
+        padding: 0px;
     }
 </style>
--- Webapp/src/services/api/blogs.js
+++ Webapp/src/services/api/blogs.js
@@ -3,5 +3,8 @@ import axios from 'axios'
 export default{
   getBlogs () {
     return axios.get('/api/blogs')
+  },
+  getBlogsTags () {
+    return axios.get('/api/blogs/tags')
   }
 }
