9e285040e33d119524930ef4eaa9669c7ddbd9b0
Kylian <kylianleemkuil@hotmail.com>
2019-01-18 11:32:04 +0100

    Added toggle blogs

--- Webapp/src/components/Admin/Settings/Settings.vue
+++ Webapp/src/components/Admin/Settings/Settings.vue
@@ -3,6 +3,19 @@
     <b-row>
             <admin-menu v-bind:active="active"></admin-menu>
             <b-col md="8">
+              <b-card class="no-scale">
+                  <div>
+                    <h2 class="text-center">Blogs</h2>
+                    <hr>
+                    <div class="text-center">
+                       <label class="font-weight-bold" for="inputDescription">Active:</label>
+                       <b-button-group size="sm">
+                          <b-button :pressed="blogActive" v-on:click="toggle()" variant="outline-primary">Yes</b-button>
+                          <b-button :pressed="!blogActive" v-on:click="toggle()" variant="outline-primary">No</b-button>
+                       </b-button-group>
+                    </div>
+                  </div>
+              </b-card>
             </b-col>
         </b-row>
     </b-container>
@@ -21,15 +34,28 @@
     },
     data() {
       return {
-        settings: []
+        active: "Settings",
+        settings: [],
+        blogActive: false
       }
     },
     methods: {
-   
+      toggle() {
+          this.blogActive = !this.blogActive;
+          let data = {
+            is_on: this.blogActive
+          };
+          settingsApi.toggleBlog(data).then(response => { });
+      }
     },
     mounted() {
       settingsApi.getSettings().then(response => { 
-        this.settings = response.data.data; 
+        this.settings = response.data.data;
+          if(this.settings[0].is_on == 1){
+            this.blogActive = true;
+          } else {
+            this.blogActive = false;
+          }
         });
     }
   }
--- Webapp/src/services/api/admin/settings.js
+++ Webapp/src/services/api/admin/settings.js
@@ -3,5 +3,8 @@ import axios from 'axios'
 export default{
     getSettings () {
         return axios.get('/api/settings')
+    },
+    toggleBlog (data) {
+        return axios.put('/api/settings/toggleblog', data)
     }
 }
